aae8975a0b912f79de9f9099fc53aa504eb97855
dev-bilal-raza <success9262@gmail.com>
2024-11-03 08:08:50 +0500

    feat: add reviews data and update dependencies in frontend

--- cv_checker_frontend/src/app/layout.tsx
+++ cv_checker_frontend/src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Header from "@/components/layout/Header"
-import Footer from "@/components/layout/Footer";
 import "./globals.css";
 
 
