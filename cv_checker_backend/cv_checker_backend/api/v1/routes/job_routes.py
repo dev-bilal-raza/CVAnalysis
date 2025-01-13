5eb44eac8e1c72321bb56c4f1885c13cd845b8e0
dev-bilal-raza <success9262@gmail.com>
2025-01-13 16:03:35 +0500

    feat: update API routes to remove versioning from endpoints and enhance error handling in authentication

--- cv_checker_backend/cv_checker_backend/api/v1/routes/job_routes.py
+++ cv_checker_backend/cv_checker_backend/api/v1/routes/job_routes.py
@@ -4,27 +4,27 @@ from cv_checker_backend.controllers.job_controller import get_all_jobs, upload_j
 
 jobRoute = APIRouter()
 
-@jobRoute.post("/api/v1/add_job")
+@jobRoute.post("/add_job")
 def addJob(message: Annotated[str, Depends(upload_job)]):
     if message:
         return message
 
-@jobRoute.get("/api/v1/get_all_jobs", response_model=dict)
+@jobRoute.get("/get_all_jobs", response_model=dict)
 def getAllJobs(all_jobs: Annotated[dict, Depends(get_all_jobs)]):
     print(all_jobs)
     return all_jobs
 
-@jobRoute.get("/api/v1/get_job_details", response_model=dict)
+@jobRoute.get("/get_job_details", response_model=dict)
 def getCVsByJob(job_details: Annotated[dict, Depends(get_cvs_by_job)]):
     if job_details:
         return job_details
 
-@jobRoute.post("/api/v1/upload_new_cvs")
+@jobRoute.post("/upload_new_cvs")
 def uploadCVs(message: Annotated[str, Depends(upload_new_cvs)]):
     if message:
         return message
 
-@jobRoute.delete("/api/v1/delete_job", response_model=dict)
+@jobRoute.delete("/delete_job", response_model=dict)
 def deleteJobs(data: Annotated[dict, Depends(delete_job)]):
     if data:
         return data
