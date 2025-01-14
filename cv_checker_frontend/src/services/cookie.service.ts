b85dc205d41e2bc277633449ee6b7d354ca7018d
dev-bilal-raza <success9262@gmail.com>
2025-01-14 16:12:02 +0500

    fix: update getToken function to return token value correctly

--- cv_checker_frontend/src/services/cookie.service.ts
+++ cv_checker_frontend/src/services/cookie.service.ts
@@ -3,5 +3,5 @@ import config from '@/common/config';
 
 export const getToken = () => {
   const token = cookies().get(config.accessTokenKey || 'access_token');
-  return token;
+  return token?.value;
 };
